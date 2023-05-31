d901bfa811453eb9741b505fb6970e4b23c96e87
Joshua Kitenge <83226114+joshuadkitenge@users.noreply.github.com>
2023-05-31 14:01:53 +0100

    add cypress run config #19

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,19 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    chromeWebSecurity: false,
+    video: false,
+    retries: {
+      runMode: 3,
+      openMode: 1,
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // `on` is used to hook into various events Cypress emits
+      // `config` is the resolved Cypress config
+      on('task', {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        failed: require('cypress-failed-log/src/failed')(),
+      });
     },
   },
 });
