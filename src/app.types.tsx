319e222f3468320836e5464e338b97002ba180e9
Joshua Kitenge <83226114+joshuadkitenge@users.noreply.github.com>
2023-06-01 13:04:16 +0100

    remove unused type #21

--- src/app.types.tsx
+++ src/app.types.tsx
@@ -1,9 +1,3 @@
 export const MicroFrontendId = 'scigateway';
 
 export type TabValue = 'Systems' | 'Catalogue' | 'Manufacturer';
-
-export interface TabPanelProps {
-  children?: React.ReactNode;
-  value: TabValue;
-  label: TabValue;
-}
