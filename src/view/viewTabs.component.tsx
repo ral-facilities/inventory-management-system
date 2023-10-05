6ccf1b41b472c4afdd31b35528d8fe3b02bd929e
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2023-10-05 11:44:11 +0100

    revert logic

--- src/view/viewTabs.component.tsx
+++ src/view/viewTabs.component.tsx
@@ -50,7 +50,10 @@ function ViewTabs() {
   }, [location.pathname, value]);
 
   React.useEffect(() => {
-    if (location.pathname === '/inventory-management-system/') {
+    if (
+      location.pathname === '/inventory-management-system/' ||
+      location.pathname === '/'
+    ) {
       navigate('/inventory-management-system/catalogue');
     }
   }, [location.pathname, navigate]);
