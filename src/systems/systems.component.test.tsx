07014330d483ed7e31362e6c8987bcfcf55253f0
Joel Davies <joel.davies@stfc.ac.uk>
2023-10-11 09:30:19 +0100

    Add test of home button

--- src/systems/systems.component.test.tsx
+++ src/systems/systems.component.test.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { renderComponentWithMemoryRouter } from '../setupTests';
 import { screen, waitFor } from '@testing-library/react';
 import Systems from './systems.component';
+import userEvent from '@testing-library/user-event';
 
 describe('Systems', () => {
+  let user;
   const createView = (path: string) => {
     return renderComponentWithMemoryRouter(<Systems />, path);
   };
+
+  beforeEach(() =>{
+    user = userEvent.setup();
+  });
 
   it('renders correctly', async () => {
     createView("/inventory-management-system/systems");
@@ -24,5 +30,22 @@ describe('Systems', () => {
     await waitFor(() => {
       expect(screen.getByText("Smaller laser")).toBeInTheDocument();
     })
+  });
+
+  it('navigates back to the root directory when home button clicked', async () => {
+    createView('/inventory-management-system/systems/giant-laser');
+
+    await waitFor(() => {
+      expect(screen.getByText('Smaller laser')).toBeInTheDocument();
+    });
+
+    const homeButton = screen.getByRole('button', {
+      name: 'navigate to systems home',
+    });
+    await user.click(homeButton);
+    await waitFor(() => {
+      expect(screen.getByText('Giant laser')).toBeInTheDocument();
+    });
+    expect(screen.getByText('Root systems')).toBeInTheDocument();
   });
 });
