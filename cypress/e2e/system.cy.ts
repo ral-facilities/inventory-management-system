a08b816114f25a954cf6eda08351aac7a69f5c95
Joel Davies <joel.davies@stfc.ac.uk>
2023-12-04 15:04:43 +0000

    Add unit tests for systemsTableView #135

--- cypress/e2e/system.cy.ts
+++ cypress/e2e/system.cy.ts
@@ -17,7 +17,7 @@ describe('System', () => {
     cy.findByText('Please select a system').should('be.visible');
 
     // Navigate deeper
-    cy.findByRole('link', { name: 'Giant laser' }).click();
+    cy.findByRole('button', { name: 'Giant laser' }).click();
     cy.url().should('include', '/systems/65328f34a40ff5301575a4e3');
     cy.findByText('No system selected').should('not.exist');
     cy.findByText('Please select a system').should('not.exist');
@@ -26,7 +26,7 @@ describe('System', () => {
     cy.findByText('Description').should('be.visible');
 
     // Navigate deeper again
-    cy.findByRole('link', { name: 'Smaller laser' }).click();
+    cy.findByRole('button', { name: 'Smaller laser' }).click();
     cy.url().should('include', '/systems/65328f34a40ff5301575a4e4');
 
     cy.findByText('Pulse Laser').should('be.visible');
