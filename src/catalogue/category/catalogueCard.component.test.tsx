87f7a3365cdc1517afd67f54e46082d9bf51fa82
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2024-01-09 13:20:02 +0000

    requested changes

--- src/catalogue/category/catalogueCard.component.test.tsx
+++ src/catalogue/category/catalogueCard.component.test.tsx
@@ -35,7 +35,7 @@ describe('Catalogue Card', () => {
     expect(screen.getByText('Beam Characterization')).toBeInTheDocument();
   });
 
-  it('opens the actions menu', async () => {
+  it.only('opens the actions menu and closes it', async () => {
     createView();
     const actionsButton = screen.getByRole('button', {
       name: 'actions Beam Characterization catalogue category button',
@@ -46,12 +46,26 @@ describe('Catalogue Card', () => {
       name: 'edit Beam Characterization catalogue category button',
     });
 
+    const saveAsButton = screen.getByRole('menuitem', {
+      name: 'save as Beam Characterization catalogue category button',
+    });
+
     const deleteButton = screen.getByRole('menuitem', {
       name: 'delete Beam Characterization catalogue category button',
     });
 
     expect(editButton).toBeVisible();
     expect(deleteButton).toBeVisible();
+    expect(saveAsButton).toBeVisible();
+
+    await user.click(editButton);
+    await user.click(
+      screen.getByRole('button', {
+        name: 'actions Beam Characterization catalogue category button',
+      })
+    );
+
+    expect(editButton).not.toBeVisible();
   });
 
   it('opens the delete dialog', async () => {
