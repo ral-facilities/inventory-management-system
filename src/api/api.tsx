1cacec1cdcc02685f395337fa0160e19f5838854
Joel Davies <joel.davies@stfc.ac.uk>
2024-02-05 17:04:50 +0000

    Add response intercept to handle token refresh #262

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import { MicroFrontendId } from '../app.types';
 import { readSciGatewayToken } from '../parseTokens';
 import { settings } from '../settings';
+import { InvalidateTokenType } from '../state/actions/actions.types';
 
 export const imsApi = axios.create();
 
@@ -10,3 +12,31 @@ imsApi.interceptors.request.use(async (config) => {
   config.headers['Authorization'] = `Bearer ${readSciGatewayToken()}`;
   return config;
 });
+
+imsApi.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const originalRequest = error.config;
+
+    // For first 403 assume token expired and attempt to retry
+    if (error.response.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      // Attempt to refresh token via SciGateway
+      document.dispatchEvent(
+        new CustomEvent(MicroFrontendId, {
+          detail: {
+            type: InvalidateTokenType,
+          },
+        })
+      );
+
+      // Update the authorisation and retry the request
+      originalRequest.headers['Authorization'] =
+        `Bearer ${readSciGatewayToken()}`;
+      imsApi(originalRequest).then((response) => response.data);
+    }
+
+    Promise.reject(error);
+  }
+);
