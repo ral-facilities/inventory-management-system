1e318e4a911bf12b7ca594a028b8d15b7ee70c2e
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2024-02-21 10:34:17 +0000

    add e2e test to check if the backend is connected #311

--- cypress/e2e/with_api/app.cy.ts
+++ cypress/e2e/with_api/app.cy.ts
@@ -3,4 +3,10 @@ describe('App', () => {
     cy.visit('/catalogue');
     cy.get('#inventory-management-system').should('be.visible');
   });
+
+  it('displays no catagories error message at root', () => {
+    cy.findByText(
+      'There are no catalogue categories. Please add a category using the plus icon in the top left of your screen'
+    ).should('exist');
+  });
 });
