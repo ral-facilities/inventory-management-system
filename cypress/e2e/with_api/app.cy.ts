2b38e23c09130614597a28613450250d99e11563
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2024-02-22 09:18:59 +0000

    test

--- cypress/e2e/with_api/app.cy.ts
+++ cypress/e2e/with_api/app.cy.ts
@@ -11,11 +11,15 @@ describe('App', () => {
     cy.intercept({
       method: 'GET',
       url: '**/catalogue-categories?parent_id=null',
-    }).as('getCatalogueCategoryData');
+    })
+      .as('getCatalogueCategoryData')
+      .debug();
     cy.visit('/catalogue');
-    cy.wait('@getCatalogueCategoryData', { timeout: 10000 });
+    cy.wait('@getCatalogueCategoryData', { timeout: 10000 }).debug();
     cy.findByText(
       'There are no catalogue categories. Please add a category using the plus icon in the top left of your screen'
-    ).should('exist');
+    )
+      .should('exist')
+      .debug();
   });
 });
