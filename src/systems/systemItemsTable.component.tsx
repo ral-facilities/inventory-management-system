9811a537f3c0ec250d0112840827203334795f2a
Joel Davies <joel.davies@stfc.ac.uk>
2024-02-23 12:44:20 +0000

    Fix groyping counts having no space on links and dates #329

--- src/systems/systemItemsTable.component.tsx
+++ src/systems/systemItemsTable.component.tsx
@@ -126,7 +126,8 @@ export function SystemItemsTable(props: SystemItemsTableProps) {
             underline="hover"
             component={Link}
             to={`/catalogue/item/${row.original.item.catalogue_item_id}`}
-            sx={{ paddingRight: 0.5 }}
+            // For ensuring space when grouping
+            sx={{ marginRight: 0.5 }}
           >
             {renderedCellValue}
           </MuiLink>
@@ -141,6 +142,8 @@ export function SystemItemsTable(props: SystemItemsTableProps) {
             underline="hover"
             component={Link}
             to={`/catalogue/item/${row.original.item.catalogue_item_id}/items/${row.original.item.id}`}
+            // For ensuring space when grouping
+            sx={{ marginRight: 0.5 }}
           >
             {renderedCellValue}
           </MuiLink>
@@ -157,7 +160,10 @@ export function SystemItemsTable(props: SystemItemsTableProps) {
         accessorKey: 'item.delivered_date',
         size: 250,
         Cell: ({ row }) => (
-          <Typography>
+          <Typography
+            // For ensuring space when grouping
+            sx={{ marginRight: 0.5 }}
+          >
             {row.original.item.delivered_date &&
               new Date(row.original.item.delivered_date).toLocaleDateString()}
           </Typography>
