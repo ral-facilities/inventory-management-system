fd5ed4e3fbe32ecedf2e12dd1e190a45812691b7
Joel Davies <joel.davies@stfc.ac.uk>
2024-02-29 16:18:32 +0000

    Exclude msw from a production build #346

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
-import { defineConfig, loadEnv } from 'vite';
+import { PluginOption, defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import fs from 'node:fs';
+
+/* See https://github.com/mswjs/msw/discussions/712 */
+function excludeMsw(): PluginOption {
+  return {
+    name: 'exclude-msw',
+    apply: 'build',
+    renderStart(outputOptions, _inputOptions) {
+      const outDir = outputOptions.dir;
+      const msWorker = path.resolve(outDir || '', 'mockServiceWorker.js');
+      fs.rm(msWorker, () => console.log(`Deleted ${msWorker}`));
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), excludeMsw()],
     server: {
       open: true,
       port: 3000,
@@ -27,12 +42,16 @@ export default defineConfig(({ command, mode }) => {
         name: 'inventory-management-system',
       },
       rollupOptions: {
+        external: ['react', 'react-dom', 'msw'],
         input: 'src/main.tsx',
         output: {
           entryFileNames: '[name].js',
           chunkFileNames: '[name].chunk.js',
           // assetFileNames: '[name].[ext]',
-          format: 'es',
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+          },
         },
         preserveEntrySignatures: 'strict',
       },
