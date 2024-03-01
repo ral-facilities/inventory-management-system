a01c1ab0bfe0bc9eb4d00b05367c8dc38ca2f5b1
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2024-03-01 11:09:29 +0000

    fix yarm e2e:api script

--- cypress/e2e/with_api/systems/functions.ts
+++ cypress/e2e/with_api/systems/functions.ts
@@ -129,9 +129,8 @@ export const moveToSystems = (values: {
   });
 
   cy.findByRole('button', { name: 'Move here' }).click();
-  cy.wait('@patchSystems', { timeout: 10000 });
   cy.findByRole('dialog').should('not.exist');
-
+  cy.wait('@patchSystems', { timeout: 10000 });
   cy.findByText('Storage').click();
   cy.wait('@getSystemsData', { timeout: 10000 });
   for (let i = 0; i < values.checkedSystems.length; i++) {
