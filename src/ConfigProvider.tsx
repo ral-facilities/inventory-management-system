9f24afcaaecc62195692165dde06a45108a61c99
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2024-03-01 13:09:06 +0000

    using function instead of class #47

--- src/ConfigProvider.tsx
+++ src/ConfigProvider.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { InventoryManagementSystemSettings, settings } from './settings';
+import {
+  InventoryManagementSystemSettings,
+  settings as IMSSettings,
+} from './settings';
 import Preloader from './preloader/preloader.component';
 
 const initialConfiguration: InventoryManagementSystemSettings = {
@@ -11,47 +14,30 @@ const initialConfiguration: InventoryManagementSystemSettings = {
 export const InventoryManagementSystemSettingsContext =
   React.createContext<InventoryManagementSystemSettings>(initialConfiguration);
 
-class ConfigProvider extends React.Component<
-  { children: React.ReactNode },
-  { loading: boolean; settings: InventoryManagementSystemSettings }
-> {
-  public constructor(props: { children: React.ReactNode }) {
-    super(props);
+function ConfigProvider({ children }: { children: React.ReactNode }) {
+  const [loading, setLoading] = React.useState(true);
+  const [settings, setSettings] =
+    React.useState<InventoryManagementSystemSettings>(initialConfiguration);
 
-    this.state = {
-      loading: true,
-      settings: initialConfiguration,
+  React.useEffect(() => {
+    const updateConfigurationState = async () => {
+      const settingsResult = await IMSSettings;
+      if (settingsResult) {
+        setLoading(false);
+        setSettings(settingsResult);
+      }
     };
-  }
 
-  public componentDidMount(): void {
-    this.updateConfigurationState();
-  }
+    updateConfigurationState();
+  }, [settings]);
 
-  private updateConfigurationState = async (): Promise<void> => {
-    const settingsResult = await settings;
-    if (settingsResult) {
-      this.setState({
-        loading: false,
-        settings: settingsResult,
-      });
-    }
-  };
-
-  public render(): React.ReactElement {
-    return (
-      // We pass the inventory management system settings that has been loaded
-      // for all child components to consume, if required.
-      <Preloader loading={this.state.loading}>
-        <InventoryManagementSystemSettingsContext.Provider
-          value={this.state.settings}
-        >
-          {this.props.children}
-        </InventoryManagementSystemSettingsContext.Provider>
-      </Preloader>
-    );
-  }
+  return (
+    <Preloader loading={loading}>
+      <InventoryManagementSystemSettingsContext.Provider value={settings}>
+        {children}
+      </InventoryManagementSystemSettingsContext.Provider>
+    </Preloader>
+  );
 }
 
-export const ConfigConsumer = InventoryManagementSystemSettingsContext.Consumer;
 export default ConfigProvider;
