e493a8a1b284bd99f3298bd95d833e768eea9eb7
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2024-03-06 10:00:48 +0000

    change the quotes

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     chromeWebSecurity: false,
     video: false,
     retries: {
-      runMode: 0,
+      runMode: 3,
       openMode: 1,
     },
     setupNodeEvents(on, config) {
