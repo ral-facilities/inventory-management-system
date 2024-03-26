e7c59f2b1401249e8190e11afd2555dd986156a0
Joel Davies <joel.davies@stfc.ac.uk>
2024-03-26 14:28:43 +0000

    Generalise further #454

--- src/common/preservedTable.component.tsx
+++ src/common/preservedTable.component.tsx
@@ -1,7 +1,7 @@
 import { MRT_ColumnFiltersState } from 'material-react-table';
 import { useSearchParams } from 'react-router-dom';
 
-// State as will be stored in search params (undefined => should not be present)
+// State as will be stored in search params (undefined => should not be present in the url)
 interface StateSearchParams {
   // Column filters
   cF?: MRT_ColumnFiltersState;
@@ -12,6 +12,14 @@ interface State {
   // Column filters
   cF: MRT_ColumnFiltersState;
 }
+
+/* This matches the definition found in tanstack table (couldn't be direcly imported
+   as its a dependency of MRT) */
+type Updater<T> = T | ((old: T) => T);
+
+/* Returns correctly types value from an updater */
+const getValueFromUpdater = <T,>(updater: Updater<T>, currentValue: T) =>
+  updater instanceof Function ? (updater(currentValue) as T) : (updater as T);
 
 export const usePreservedTableState = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -27,23 +35,27 @@ export const usePreservedTableState = () => {
     cF: parsedStateSearchParams.cF || [],
   };
 
-  console.log(unparsedStateSearchParams);
-
-  const setColumnFilters = (
-    updaterOrValue:
-      | MRT_ColumnFiltersState
-      | ((old: MRT_ColumnFiltersState) => MRT_ColumnFiltersState)
-  ) => {
-    const newColumnFilters =
-      updaterOrValue instanceof Function
-        ? updaterOrValue(state.cF)
-        : updaterOrValue;
-
+  const updateSearchParams = (modifiedParams: StateSearchParams) => {
+    // Merge existing and new params
     const newStateSearchParams: StateSearchParams = {
       ...parsedStateSearchParams,
-      cF: newColumnFilters.length === 0 ? undefined : newColumnFilters,
+      ...modifiedParams,
     };
-    setSearchParams({ state: JSON.stringify(newStateSearchParams) });
+    const newUnparsedStateSearchParams = JSON.stringify(newStateSearchParams);
+
+    // Clear search params if state is no longer needed
+    if (newUnparsedStateSearchParams !== '{}')
+      setSearchParams({ state: newUnparsedStateSearchParams });
+    else setSearchParams({});
+  };
+
+  const setColumnFilters = (
+    updaterOrValue: Updater<MRT_ColumnFiltersState>
+  ) => {
+    const newColumnFilters = getValueFromUpdater(updaterOrValue, state.cF);
+    updateSearchParams({
+      cF: newColumnFilters.length === 0 ? undefined : newColumnFilters,
+    });
   };
 
   return {
