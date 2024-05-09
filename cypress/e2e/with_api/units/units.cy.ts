9d4a4b491a56842dd050cc5030c697368d952fdc
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2024-05-09 09:46:04 +0100

    added API e2e tests #536

---
+++ cypress/e2e/with_api/units/units.cy.ts
@@ -0,0 +1,37 @@
+import { addUnits, deleteUnits } from './functions';
+
+describe('Units', () => {
+  beforeEach(() => {
+    cy.dropIMSCollections(['units']);
+    cy.visit('/adminPage/units');
+  });
+  afterEach(() => {
+    cy.clearMocks();
+    cy.dropIMSCollections(['units']);
+  });
+
+  it('CRD for units', () => {
+    addUnits([
+      'megapixels',
+      'fps',
+      'Joules',
+      'micrometers',
+      'millimeters',
+      'kilograms',
+      'liters per second',
+      'millibar',
+      'volts',
+    ]);
+    deleteUnits([
+      'megapixels',
+      'fps',
+      'Joules',
+      'micrometers',
+      'millimeters',
+      'kilograms',
+      'liters per second',
+      'millibar',
+      'volts',
+    ]);
+  });
+});
