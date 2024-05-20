8973fcb9152104c74137a0e8991ff9680a9957ed
MatteoGuarnaccia5 <matteo.guarnaccia@stfc.ac.uk>
2024-05-20 12:21:35 +0000

    fixed tests #533 and renamed e2e file

--- cypress/e2e/with_mock_data/admin.cy.ts
+++ cypress/e2e/with_mock_data/admin.cy.ts
@@ -5,6 +5,6 @@ describe('Admin Page', () => {
 
   it('should render admin page correctly', () => {
     cy.findByText('Units').should('be.visible');
-    cy.findByText('Usage Status').should('be.visible');
+    cy.findByText('Usage Statuses').should('be.visible');
   });
 });
