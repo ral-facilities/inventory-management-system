8a555e21f888a30bc3b16321c87bfa8e8c8423b4
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2024-06-13 16:30:37 +0000

    Fix typescript warnings in systemItemsTable.test.tsx #653

--- src/systems/systemItemsTable.component.test.tsx
+++ src/systems/systemItemsTable.component.test.tsx
@@ -78,7 +78,7 @@ describe('SystemItemsTable', () => {
     });
 
     it('renders correctly when there are no items to display', async () => {
-      props.system = { ...props.system, id: 'invalid' };
+      props.system = { ...props.system, id: 'invalid' } as System;
 
       createView();
 
@@ -486,8 +486,8 @@ describe('SystemItemsTable', () => {
         { timeout: 4000 }
       );
 
-      expect(screen.getAllByRole('combobox')[0].value).toBe('New');
-      expect(screen.getAllByRole('combobox')[1].value).toBe('In Use');
+      expect(screen.getAllByRole('combobox')[0]).toHaveValue('New');
+      expect(screen.getAllByRole('combobox')[1]).toHaveValue('In Use');
     });
 
     it('displays errors messages correctly', async () => {
