6824c81c0ffa5d21fcc6a295056bcb26ab42c8ec
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2024-06-13 14:27:47 +0000

    Fix typescript warnings in deleteUnitsDialog.test.tsx #653

--- src/admin/units/deleteUnitsDialog.component.test.tsx
+++ src/admin/units/deleteUnitsDialog.component.test.tsx
@@ -24,6 +24,7 @@ describe('delete Unit dialog', () => {
     (unit = {
       id: '1',
       value: 'test',
+      code: 'test',
       created_time: '2024-01-01T12:00:00.000+00:00',
       modified_time: '2024-01-02T13:10:10.000+00:00',
     }),
