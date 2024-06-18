326ce0121b83a622c72ddb0f30cb916229e1b35a
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2024-06-18 06:39:49 +0000

    Use props (objects) for the zodSchema props #663

--- src/form.schemas.tsx
+++ src/form.schemas.tsx
@@ -2,47 +2,61 @@ import { z } from 'zod';
 
 export type RequestType = 'post' | 'patch';
 
+interface BaseZodSchemaProps {
+  errorMessage?: string;
+}
+
+interface PostPatchZodSchemaProps extends BaseZodSchemaProps {
+  requestType: RequestType;
+}
+
 const OptionalStringSchema = z.string().trim().optional();
 
-const OptionalOrNullableStringSchema = (requestType: RequestType) =>
-  requestType === 'post'
+const OptionalOrNullableStringSchema = (props: PostPatchZodSchemaProps) =>
+  props.requestType === 'post'
     ? OptionalStringSchema.transform((val) => (!val ? undefined : val))
     : OptionalStringSchema.transform((val) => (!val ? null : val)).nullable();
 
-const MandatoryStringSchema = (errorMessage: string) =>
+const MandatoryStringSchema = (props: BaseZodSchemaProps) =>
   z
     .string({
-      required_error: errorMessage,
+      required_error: props.errorMessage,
     })
     .trim()
-    .min(1, { message: errorMessage });
+    .min(1, { message: props.errorMessage });
 
-const OptionalUrlSchema = (errorMessage: string) =>
-  z.string().trim().url({ message: errorMessage }).optional();
+const OptionalUrlSchema = (props: BaseZodSchemaProps) =>
+  z.string().trim().url({ message: props.errorMessage }).optional();
 
-const OptionalOrNullableURLSchema = (
-  requestType: RequestType,
-  errorMessage: string
-) =>
-  requestType === 'post'
-    ? OptionalUrlSchema(errorMessage).or(
+const OptionalOrNullableURLSchema = (props: PostPatchZodSchemaProps) =>
+  props.requestType === 'post'
+    ? OptionalUrlSchema({ errorMessage: props.errorMessage }).or(
         z.literal('').transform(() => undefined)
       )
-    : OptionalUrlSchema(errorMessage)
+    : OptionalUrlSchema({ errorMessage: props.errorMessage })
         .or(z.literal('').transform(() => null))
         .nullable();
 
 export const ManufacturerSchema = (requestType: RequestType) => {
   return z.object({
-    name: MandatoryStringSchema('Please enter a name.'),
-    url: OptionalOrNullableURLSchema(requestType, 'Please enter a valid URL.'),
-    address: z.object({
-      address_line: MandatoryStringSchema('Please enter an address.'),
-      town: OptionalOrNullableStringSchema(requestType),
-      county: OptionalOrNullableStringSchema(requestType),
-      postcode: MandatoryStringSchema('Please enter a post code or zip code.'),
-      country: MandatoryStringSchema('Please enter a country.'),
+    name: MandatoryStringSchema({ errorMessage: 'Please enter a name.' }),
+    url: OptionalOrNullableURLSchema({
+      requestType,
+      errorMessage: 'Please enter a valid URL.',
     }),
-    telephone: OptionalOrNullableStringSchema(requestType),
+    address: z.object({
+      address_line: MandatoryStringSchema({
+        errorMessage: 'Please enter an address.',
+      }),
+      town: OptionalOrNullableStringSchema({ requestType }),
+      county: OptionalOrNullableStringSchema({ requestType }),
+      postcode: MandatoryStringSchema({
+        errorMessage: 'Please enter a post code or zip code.',
+      }),
+      country: MandatoryStringSchema({
+        errorMessage: 'Please enter a country.',
+      }),
+    }),
+    telephone: OptionalOrNullableStringSchema({ requestType }),
   });
 };
