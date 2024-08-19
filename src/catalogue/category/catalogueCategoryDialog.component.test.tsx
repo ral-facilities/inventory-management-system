b6c9c2964430c5e97013ce6333b641e58c3f7bfa
Anikesh Suresh <sureshanikesh@yahoo.co.uk>
2024-08-19 18:59:10 +0000

    remove unused import

--- src/catalogue/category/catalogueCategoryDialog.component.test.tsx
+++ src/catalogue/category/catalogueCategoryDialog.component.test.tsx
@@ -17,7 +17,6 @@ import { resetUniqueIdCounter } from '../../utils';
 import CatalogueCategoryDialog, {
   CatalogueCategoryDialogProps,
 } from './catalogueCategoryDialog.component';
-import { act } from 'react';
 
 vi.mock('../../handleIMS_APIError');
 
