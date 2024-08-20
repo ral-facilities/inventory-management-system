a87faff83f5f7af35aad9f273c41043341197096
Anikesh Suresh <sureshanikesh@yahoo.co.uk>
2024-08-20 08:08:56 +0000

    Revert "add console.log to debug CI test statements to see what is not wrapped with act"
    This reverts commit 02407a08b519eec5183a7b973b6959849a770d7e.

--- src/catalogue/category/catalogueCategoryDialog.component.test.tsx
+++ src/catalogue/category/catalogueCategoryDialog.component.test.tsx
@@ -318,7 +318,6 @@ describe('Catalogue Category Dialog', () => {
     it('create a catalogue category with content being catalogue items', async () => {
       createView();
 
-      console.log("Before modifyValues");
       await modifyValues({
         name: 'test',
         newFormFields: [
@@ -330,15 +329,12 @@ describe('Catalogue Category Dialog', () => {
           },
         ],
       });
-      console.log("After modifyValues");
 
       expect(screen.getByText('Catalogue Item Fields')).toBeInTheDocument();
 
       const saveButton = screen.getByRole('button', { name: 'Save' });
-      
-      console.log("Before clicking save button");
+
       await waitFor(() => user.click(saveButton));
-      console.log("After clicking save button");
 
       expect(axiosPostSpy).toHaveBeenCalledWith('/v1/catalogue-categories', {
         properties: [
