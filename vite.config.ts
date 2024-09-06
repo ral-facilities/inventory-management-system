dd44efe5e41b8263ba5690bca9bdab2bd9d2366f
Joel Davies <joel.davies@stfc.ac.uk>
2024-09-06 10:41:58 +0000

    Remove main.test.tsx again to avoid unnecessary test warning

--- vite.config.ts
+++ vite.config.ts
@@ -159,6 +159,7 @@ export default defineConfig(({ mode }) => {
           'src/mocks/browser.ts',
           'src/mocks/server.ts',
           'src/vite-env.d.ts',
+          'src/main.tsx',
         ],
       },
     },
