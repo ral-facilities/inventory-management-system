18be625631f6b2ebb6de67d2abb8ea8eef5879f8
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2025-03-20 20:02:26 +0000

    Fix typing error #1066

--- src/common/downloadFileDialog.component.tsx
+++ src/common/downloadFileDialog.component.tsx
@@ -8,7 +8,12 @@ import {
 import { useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import React from 'react';
-import { APIImage, APIImageWithURL, AttachmentMetadata, AttachmentMetadataWithURL } from '../api/api.types';
+import {
+  APIImage,
+  APIImageWithURL,
+  AttachmentMetadata,
+  AttachmentMetadataWithURL,
+} from '../api/api.types';
 import { getAttachmentQuery } from '../api/attachments';
 import { getImageQuery } from '../api/images';
 import handleIMS_APIError from '../handleIMS_APIError';
@@ -27,37 +32,51 @@ export interface ImageDownloadDialogProps extends BaseDownloadFileProps {
 
 export interface AttachmentDownloadDialogProps extends BaseDownloadFileProps {
   fileType: 'Attachment';
-  file: AttachmentMetadata | undefined;
+  file: AttachmentMetadata;
 }
 
-export type DownloadFileProps = ImageDownloadDialogProps | AttachmentDownloadDialogProps;
+export type DownloadFileProps =
+  | ImageDownloadDialogProps
+  | AttachmentDownloadDialogProps;
 
 const DownloadFileDialog = (props: DownloadFileProps) => {
   const { open, onClose, fileType, file } = props;
 
-  const getDownloadFileQuery = fileType === 'Image' ? getImageQuery : getAttachmentQuery;
-
   const queryClient = useQueryClient();
 
   const handleClick = React.useCallback(async () => {
-    file &&
-    queryClient
-      .fetchQuery(getDownloadFileQuery(file.id, false))
-      .then((data: APIImageWithURL | AttachmentMetadataWithURL) => {
-        downloadFileByLink(data.download_url, data.file_name);
-        onClose();
-      })
-      .catch((error: AxiosError) => {
-        handleIMS_APIError(error);
-        onClose();
-      });
-  }, [file, queryClient, onClose, getAttachmentQuery, getImageQuery]);
+    if (file) {
+      if (fileType === 'Image') {
+        queryClient
+          .fetchQuery(getImageQuery(file.id, false))
+          .then((data: APIImageWithURL) => {
+            downloadFileByLink(data.download_url, data.file_name);
+            onClose();
+          })
+          .catch((error: AxiosError) => {
+            handleIMS_APIError(error);
+            onClose();
+          });
+      } else {
+        queryClient
+          .fetchQuery(getAttachmentQuery(file.id, false))
+          .then((data: AttachmentMetadataWithURL) => {
+            downloadFileByLink(data.download_url, data.file_name);
+            onClose();
+          })
+          .catch((error: AxiosError) => {
+            handleIMS_APIError(error);
+            onClose();
+          });
+      }
+    }
+  }, [file, fileType, queryClient, onClose]);
 
   return (
     <Dialog open={open} maxWidth="lg">
       <DialogTitle>Download {fileType}?</DialogTitle>
       <DialogContent>
-        Are you sure you want to download <strong>{file?.file_name}</strong>?
+        Are you sure you want to download <strong>{file.file_name}</strong>?
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose}>Cancel</Button>
