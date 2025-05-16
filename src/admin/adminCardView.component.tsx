b1280db44a83972a4aa0871fd60f3b224cf50333
Anikesh Suresh <sureshanikesh@yahoo.co.uk>
2025-05-16 10:33:53 +0000

    Fix admin page card layout #1416

--- src/admin/adminCardView.component.tsx
+++ src/admin/adminCardView.component.tsx
@@ -5,86 +5,84 @@ import { Link } from 'react-router-dom';
 function AdminCardView() {
   return (
     <Grid container>
-      <Grid container flexDirection={'column'}>
-        <Grid container xs={12} overflow={'auto'}>
-          <Grid key={0} xs={12} sm={6}>
-            <Button
-              component={Link}
-              to="units"
-              fullWidth
+      <Grid container xs={12} overflow={'auto'}>
+        <Grid key={0} xs={12} sm={6}>
+          <Button
+            component={Link}
+            to="units"
+            fullWidth
+            sx={{
+              display: 'flex',
+              width: '100%',
+              textDecoration: 'none',
+              color: 'inherit',
+              position: 'relative', // Make the parent container relative
+            }}
+          >
+            <Card
               sx={{
-                display: 'flex',
+                padding: 1,
                 width: '100%',
-                textDecoration: 'none',
-                color: 'inherit',
-                position: 'relative', // Make the parent container relative
+                display: 'flex',
+                height: '100px', // Set a fixed height for all cards
               }}
             >
-              <Card
+              <CardContent
                 sx={{
-                  padding: 1,
                   width: '100%',
                   display: 'flex',
-                  height: '100px', // Set a fixed height for all cards
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  minWidth: 0,
                 }}
               >
-                <CardContent
-                  sx={{
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    minWidth: 0,
-                  }}
-                >
-                  <Grid>
-                    <Grid position="relative">
-                      <Typography>Units</Typography>
-                    </Grid>
+                <Grid>
+                  <Grid position="relative">
+                    <Typography>Units</Typography>
                   </Grid>
-                </CardContent>
-              </Card>
-            </Button>
-          </Grid>
-          <Grid key={1} xs={12} sm={6}>
-            <Button
-              component={Link}
-              to={'usage-statuses'}
-              fullWidth
+                </Grid>
+              </CardContent>
+            </Card>
+          </Button>
+        </Grid>
+        <Grid key={1} xs={12} sm={6}>
+          <Button
+            component={Link}
+            to={'usage-statuses'}
+            fullWidth
+            sx={{
+              display: 'flex',
+              width: '100%',
+              textDecoration: 'none',
+              color: 'inherit',
+              position: 'relative',
+            }}
+          >
+            <Card
               sx={{
-                display: 'flex',
+                padding: 1,
                 width: '100%',
-                textDecoration: 'none',
-                color: 'inherit',
-                position: 'relative',
+                display: 'flex',
+                height: '100px',
               }}
             >
-              <Card
+              <CardContent
                 sx={{
-                  padding: 1,
                   width: '100%',
                   display: 'flex',
-                  height: '100px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  minWidth: 0,
                 }}
               >
-                <CardContent
-                  sx={{
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    minWidth: 0,
-                  }}
-                >
-                  <Grid>
-                    <Grid position="relative">
-                      <Typography>Usage Statuses</Typography>
-                    </Grid>
+                <Grid>
+                  <Grid position="relative">
+                    <Typography>Usage Statuses</Typography>
                   </Grid>
-                </CardContent>
-              </Card>
-            </Button>
-          </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Button>
         </Grid>
       </Grid>
     </Grid>
