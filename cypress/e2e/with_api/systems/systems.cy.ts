94b39acdd641c3246588f28d3a54b914a5bcd018
MatteoGuarnaccia5 <matteo.guarnaccia@stfc.ac.uk>
2025-09-29 13:22:58 +0000

    fixed e2e tests ()ith mock data)

--- cypress/e2e/with_api/systems/systems.cy.ts
+++ cypress/e2e/with_api/systems/systems.cy.ts
@@ -18,7 +18,7 @@ describe('systems', () => {
   beforeEach(() => {
     cy.window().then((win) => {
       win.localStorage.setItem(
-        'MicroFrontendToken',
+        'scigateway:token',
         'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwicm9sZXMiOlsiYWRtaW4iXSwidXNlcklzQWRtaW4iOnRydWUsImV4cCI6MjUzNDAyMzAwNzk5fQ.gWXkZNeLCgNA04KhkGcAUB8WwrrVr8HMKp8yd9BUEBfDuiN1yekPxwKJ7LZDndHqYL4z9WWfVsDE5vYyWfjDJjhoymuP-VYTAI2GxbmazRmknsl9L-vRo31oPX3v2Cs5V2tcBv7dM49gzY7w-dS0b9QsOrn4Y1z9zLj4kLpVtNm0EhtbwThxMk8qVNNtEu76TAnYrdWAoz7_IedBh9NRf48EKJFfoh4CSbfXhHsGRZjvAKnjU-khaibWP3aWuMzN1nwQJ8WasgvhPaxMxd1qzKTbfpMMjg2eo3hDcQogU545P8zO4PcfzIid1g9hF1vMgRsAtQNK385oqBjYfOOWZw'
       );
     });
