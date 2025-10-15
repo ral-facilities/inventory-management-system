1cfeebf2da2e09e6b9b4f53a848b80b8e8da2090
MatteoGuarnaccia5 <matteo.guarnaccia@stfc.ac.uk>
2025-10-15 10:04:58 +0000

    fix cy.visit url in e2e api tests

--- cypress/e2e/with_api/systemTypes/systemTypes.cy.ts
+++ cypress/e2e/with_api/systemTypes/systemTypes.cy.ts
@@ -6,7 +6,7 @@ describe('Usage Statuses', () => {
         'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwicm9sZSI6ImFkbWluIiwidXNlcklzQWRtaW4iOnRydWUsImV4cCI6MjUzNDAyMzAwNzk5fQ.e_yNd4axueRx9_4rG05tWNHiUkwsoZUsNdpl8vb5ofHiFkJAB7D2Gy6NJmg9Pg4fKxpGS-HqRfCjrtQiWX-ZM3UCJ3S468bWk_DEpEeift3wfp8Kmha3iEgAYruMta7RaoWeeyYMVqq581zHhb8zCquMfFz30R-VKZw_MQidvhK1G3QpwAs-kwcCLgugZi3C2kw5JBDm_jQlyyGiK06C_X5c4tGSvpgMFz0ex6gAr6QcEX9kkS7TKrLySoL5DC_ElKrjOs24QhPO2xlKOw82rfJa7wRpARWFdbY0NFy7veAiQfzlfW_9X_Mas2gRMF6tu6pkTnVRoLIv07l-nukjlA'
       );
     });
-    cy.visit('/admin-ims/system-types');
+    cy.visit('/settings/system-types');
   });
   afterEach(() => {
     cy.clearMocks();
