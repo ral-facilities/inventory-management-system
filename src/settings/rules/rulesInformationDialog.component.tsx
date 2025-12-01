c5db93697a6f33a3f79b15dcabd3f6a5923ba463
Lottie Oldroyd <lottie.oldroyd@stfc.ac.uk>
2025-12-01 16:35:41 +0000

    Add rule examples with accordions #1494

--- src/settings/rules/rulesInformationDialog.component.tsx
+++ src/settings/rules/rulesInformationDialog.component.tsx
@@ -1,4 +1,8 @@
+import { ExpandMoreOutlined } from '@mui/icons-material';
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Button,
   Dialog,
   DialogActions,
@@ -7,6 +11,39 @@ import {
   Divider,
   Typography,
 } from '@mui/material';
+
+interface RuleAccordionProps {
+  summary: string;
+  details: string;
+}
+
+const AccordionContent = ({ summary, details }: RuleAccordionProps) => {
+  const accordionProps = {
+    sx: {
+      marginLeft: 3,
+      boxShadow: 0,
+      '&:before': { display: 'none' },
+    },
+  };
+
+  const accordionSummaryProps = {
+    expandIcon: <ExpandMoreOutlined sx={{ marginRight: 2 }} />,
+    sx: {
+      flexDirection: 'row-reverse',
+    },
+  };
+
+  const accordionDetailsProps = {
+    sx: { marginLeft: 5, fontSize: '0.9rem' },
+  };
+
+  return (
+    <Accordion {...accordionProps}>
+      <AccordionSummary {...accordionSummaryProps}>{summary}</AccordionSummary>
+      <AccordionDetails {...accordionDetailsProps}>{details}</AccordionDetails>
+    </Accordion>
+  );
+};
 
 export interface RulesInformationDialogProps {
   open: boolean;
@@ -20,7 +57,7 @@ const RulesInformationDialog = (props: RulesInformationDialogProps) => {
     <Dialog
       sx={{ zIndex: 1210 + 10 }}
       open={open}
-      maxWidth="sm"
+      maxWidth="md"
       disableEnforceFocus
       fullWidth
     >
@@ -30,20 +67,33 @@ const RulesInformationDialog = (props: RulesInformationDialogProps) => {
           Rules define what operations are possible for items in relation to
           systems.
           <p />
-        </Typography>
-        <Divider></Divider>
-        <Typography variant="body1">
+          <Divider></Divider>
           <p />
           They are split into three categories:
           <p />
           <b>Creation:</b> These define which types of system's items can be
           created in, and what usage status they should have.
+          <AccordionContent
+            summary="Example: Storage → 'New'"
+            details="Items can be created in the system type 'Storage'. This would set
+            the items usage status to 'New'."
+          />
           <p />
           <b>Deletion:</b> These define which types of system's items can be
           deleted from. (They have no restriction on usage status).
+          <AccordionContent
+            summary="Example: Operational → Scrapped → 'Scrapped'"
+            details="Items can be moved from the system type 'Operational' to 'Scrapped'. This would set
+            the items usage status to 'Scrapped'."
+          />
           <p />
           <b>Moving:</b> These define which types of systems an item is allowed
           to move between and what usage status they should have once moved.
+          <AccordionContent
+            summary="Example: Storage → Operational → 'In Use'"
+            details="Items can be moved from the system type 'Storage' to 'Operational'. This would set
+            the items usage status to 'In Use'."
+          />
           <p />
           These rules are defined in terms of the source system's type,
           destination system's type, and usage status.
