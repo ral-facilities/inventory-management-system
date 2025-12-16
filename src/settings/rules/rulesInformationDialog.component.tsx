65e52a1cbcc98b418abf7c84cf5cca6b820d5372
Lottie Oldroyd <lottie.oldroyd@stfc.ac.uk>
2025-12-16 14:57:47 +0000

    Restyle information dialog content #1494

--- src/settings/rules/rulesInformationDialog.component.tsx
+++ src/settings/rules/rulesInformationDialog.component.tsx
@@ -20,27 +20,30 @@ interface RuleAccordionProps {
 const AccordionContent = ({ summary, details }: RuleAccordionProps) => {
   const accordionProps = {
     sx: {
-      marginLeft: 3,
       boxShadow: 0,
       '&:before': { display: 'none' },
     },
   };
 
   const accordionSummaryProps = {
-    expandIcon: <ExpandMoreOutlined sx={{ marginRight: 2 }} />,
+    expandIcon: <ExpandMoreOutlined />,
     sx: {
       flexDirection: 'row-reverse',
     },
   };
 
   const accordionDetailsProps = {
-    sx: { marginLeft: 5, fontSize: '0.9rem' },
+    sx: { ml: 3 },
   };
 
   return (
-    <Accordion {...accordionProps}>
-      <AccordionSummary {...accordionSummaryProps}>{summary}</AccordionSummary>
-      <AccordionDetails {...accordionDetailsProps}>{details}</AccordionDetails>
+    <Accordion disableGutters {...accordionProps}>
+      <AccordionSummary {...accordionSummaryProps}>
+        <Typography>{summary}</Typography>
+      </AccordionSummary>
+      <AccordionDetails {...accordionDetailsProps}>
+        <Typography variant="body2">{details}</Typography>
+      </AccordionDetails>
     </Accordion>
   );
 };
@@ -57,30 +60,30 @@ const RulesInformationDialog = (props: RulesInformationDialogProps) => {
     <Dialog
       sx={{ zIndex: 1210 + 10 }}
       open={open}
-      maxWidth="md"
+      maxWidth="sm"
       disableEnforceFocus
       fullWidth
     >
       <DialogTitle>Rules Information</DialogTitle>
       <DialogContent>
-        <Typography variant="body1">
+        <Typography>
           Rules define what operations are possible for items in relation to
           systems.
         </Typography>
-        <Divider></Divider>
-        <Typography variant="body1">
+        <Divider sx={{ my: 1 }}></Divider>
+        <Typography sx={{ mb: 2 }}>
           They are split into three categories:
         </Typography>
-        <Typography variant="body1">
+        <Typography>
           <b>Creation:</b> These define which types of system&apos;s items can
           be created in, and what usage status they should have.
         </Typography>
         <AccordionContent
           summary="Example: Storage → 'New'"
-          details="Items can be created in the system type 'Storage'. This would set
+          details="Items can be created in the system type 'Storage'. This rule sets
             the items usage status to 'New'."
         />
-        <Typography variant="body1">
+        <Typography>
           <b>Deletion:</b> These define which types of system&apos;s items can
           be deleted from. (They have no restriction on usage status).
         </Typography>
@@ -88,16 +91,16 @@ const RulesInformationDialog = (props: RulesInformationDialogProps) => {
           summary="Example: Storage"
           details="Items can be deleted from the system type 'Storage'."
         />
-        <Typography variant="body1">
+        <Typography>
           <b>Moving:</b> These define which types of systems an item is allowed
           to move between and what usage status they should have once moved.
         </Typography>
         <AccordionContent
           summary="Example: Storage → Operational → 'In Use'"
-          details="Items can be moved from the system type 'Storage' to 'Operational'. This would set
+          details="Items can be moved from the system type 'Storage' to 'Operational'. This rule sets
             the items usage status to 'In Use'."
         />
-        <Typography variant="body1">
+        <Typography>
           These rules are defined in terms of the source system&apos;s type,
           destination system&apos;s type, and usage status.
         </Typography>
