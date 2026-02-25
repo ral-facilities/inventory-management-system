b6027c220c050972d73059a67d0eaf7e36890a57
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2026-02-25 14:53:00 +0000

    undo unwanted change #1638

--- src/items/itemDialog.component.tsx
+++ src/items/itemDialog.component.tsx
@@ -20,7 +20,7 @@ import {
   Typography,
 } from '@mui/material';
 import Grid from '@mui/material/Grid2';
-import { DateValidationError, DesktopDatePicker } from '@mui/x-date-pickers';
+import { DatePicker, DateValidationError } from '@mui/x-date-pickers';
 import { AxiosError } from 'axios';
 import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
@@ -856,7 +856,7 @@ function ItemDialog(props: ItemDialogProps) {
                 name="warranty_end_date"
                 control={controlDetailsStep}
                 render={({ field }) => (
-                  <DesktopDatePicker
+                  <DatePicker
                     label="Warranty end date"
                     {...field}
                     value={
@@ -910,7 +910,7 @@ function ItemDialog(props: ItemDialogProps) {
                 name="delivered_date"
                 control={controlDetailsStep}
                 render={({ field }) => (
-                  <DesktopDatePicker
+                  <DatePicker
                     label="Delivered date"
                     {...field}
                     value={
