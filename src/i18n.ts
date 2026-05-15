4aedc0c36ea845185396cd1850df42252c3d5a51
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2026-05-15 14:55:09 +0100

    Add i18next for home page #1788

---
+++ src/i18n.ts
@@ -0,0 +1,27 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+const loadPath = import.meta.env
+  .VITE_INVENTORY_MANAGEMENT_SYSTEM_BUILD_DIRECTORY
+  ? import.meta.env.VITE_INVENTORY_MANAGEMENT_SYSTEM_BUILD_DIRECTORY +
+    'res/default.json'
+  : '/res/default.json';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: import.meta.env.DEV,
+    backend: {
+      loadPath: loadPath,
+    },
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
+
+export default i18n;
