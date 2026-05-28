b366a84a189d3cc0083b5d61758b1295fdd8a5e0
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2026-05-28 12:53:54 +0100

    add msw endpoints for validate #1807

--- src/mocks/serverHandler.ts
+++ src/mocks/serverHandler.ts
@@ -36,4 +36,34 @@ export const serverHandlers = [
       },
     });
   }),
+
+  http.post<
+    PathParams,
+    { catalogue_category_id: string },
+    ErrorResponse | Blob
+  >('/spreadsheets/catalogue-items/validate', async ({ request }) => {
+    const data = await request.formData();
+    const catalogueCategoryId = data.get('catalogue_category_id');
+
+    const fileBuffer = fs.readFileSync(filePath);
+
+    const blob = new Blob([fileBuffer], {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    });
+
+    const catalogueCategory = CatalogueCategoriesJSON.find(
+      (val) => val.id === catalogueCategoryId
+    );
+    return new HttpResponse(blob, {
+      status: 200,
+      headers: {
+        'Content-Type':
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'Content-Disposition': `attachment; filename="CatalogueItemTemplate-${catalogueCategory?.name}-Validated.xlsx"`,
+        'IMSIngestAPI-Validation-Warnings': '0',
+        'IMSIngestAPI-Validation-Errors': '0',
+        'IMSIngestAPI-Validation-Valid': 'true',
+      },
+    });
+  }),
 ];
