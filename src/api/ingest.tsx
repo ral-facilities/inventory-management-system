4dca7f8bf717818cb78708b7387a5e5a5b637921
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2026-06-17 11:55:37 +0100

    address review comments #1794

--- src/api/ingest.tsx
+++ src/api/ingest.tsx
@@ -1,13 +1,11 @@
-import {
-  useMutation,
-  UseMutationResult,
-  useQueryClient,
-} from '@tanstack/react-query';
+import { useMutation, UseMutationResult } from '@tanstack/react-query';
 import { AxiosError, AxiosResponse } from 'axios';
 import { ingestApi } from './api';
 
 const postCatalogueItemsTemplate = async (
   catalogueCategoryId: string
+  // Blob response required because direct browser requests auto-download based on headers, but programmatic requests
+  // (fetch/axios) return data to JavaScript, so the client must manually trigger the download
 ): Promise<AxiosResponse<Blob>> => {
   return ingestApi.post(
     '/spreadsheets/catalogue-items/template',
@@ -23,12 +21,8 @@ export const usePostCatalogueItemsTemplate = (): UseMutationResult<
   AxiosError,
   { catalogueCategoryId: string }
 > => {
-  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: ({ catalogueCategoryId }: { catalogueCategoryId: string }) =>
       postCatalogueItemsTemplate(catalogueCategoryId),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['CatalogueItems'] });
-    },
   });
 };
