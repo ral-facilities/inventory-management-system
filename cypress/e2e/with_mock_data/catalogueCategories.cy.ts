52dcb27b35b0bc793a3165317b14b89e82e07575
MatteoGuarnaccia5 <matteo.guarnaccia@stfc.ac.uk>
2026-09-22 07:16:14 +0000

    increase test timeouts

--- cypress/e2e/with_mock_data/catalogueCategories.cy.ts
+++ cypress/e2e/with_mock_data/catalogueCategories.cy.ts
@@ -940,7 +940,7 @@ describe('Catalogue Category', () => {
           created_time: '2024-01-01T12:00:00.000+00:00',
           modified_time: '2024-01-02T13:10:10.000+00:00',
           modified_comment: 'category name updated',
-          modified_by: 'Sarah Jenkins',
+          modified_by: 'Alex Chen',
           is_flagged: false,
         })
       );
