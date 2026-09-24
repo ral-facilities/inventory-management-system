f4ea57a67db7404c462dcf1e932ed2302eaabc25
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2026-09-24 13:55:41 +0100

    remove debugging for coverage

--- vite.config.ts
+++ vite.config.ts
@@ -141,7 +141,7 @@ export default defineConfig(({ mode }) => {
       globals: true,
       environment: 'jsdom',
       globalSetup: './globalSetup.js',
-      testTimeout: 10000, // 10 seconds
+      testTimeout: 10000,
       setupFiles: ['src/setupTests.ts'],
       coverage: {
         reporter: [
